136787ed1b32feb1450c112545a77cade27b3c60
Michael Dean Haynie <michael.dean.haynie@gmail.com>
2019-11-16 07:51:09 -0500

    unfocus 'GoalRetroComponent' test suite

--- src/app/components/goal-retro/goal-retro.component.spec.ts
+++ src/app/components/goal-retro/goal-retro.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GoalRetroComponent } from './goal-retro.component';
 
-fdescribe('GoalRetroComponent', () => {
+describe('GoalRetroComponent', () => {
     let component: GoalRetroComponent;
     let fixture: ComponentFixture<GoalRetroComponent>;
 
